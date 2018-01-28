2970a5c47095ab320f15a45cb87dcf2cd3c32675
TrueVit <truevit@gmail.com>
2018-01-28 11:13:46 +0300

    Initial commit

---
+++ testHTTP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace testHTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(DownloadPageAsync);
+            t.Start();
+            Console.WriteLine("Downloading page...");
+            Console.ReadLine();
+        }
+        static async void DownloadPageAsync()
+        {
+            // ... Target page.
+            string page = "https://virtonomica.ru";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(page))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                // ... Display the result.
+                if (result != null &&
+                    result.Length >= 50)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+
+
+        }
+    }
+}
